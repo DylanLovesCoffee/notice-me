9df753451756e3a46fb80600ab005d79df7a3e5a
Dylan Yang <Dylan@Dylans-MacBook-Air.local>
2017-09-03 12:41:21 -0400

    Create route to home

---
+++ src/containers/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Notice Me</h1>
+          <p>Take notes about how much senpai is ignoring you.</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
